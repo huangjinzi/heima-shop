6cdf76497c6e5e1107ae890b08b7372c1c99f1b7
como <jivedon@163.com>
2024-04-10 22:05:08 +0800

    分类&推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,9 @@ import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import * as CustomNavbar from './components/CustomNavbar.vue'
 import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import * as CategoryPanel from './components/CategoryPanel.vue'
+import * as HotPanel from './components/HotPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -13,16 +15,44 @@ const getHomeBannerData = async () => {
   bannerList.value = res.result
 }
 
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 获取热门推荐数据
+const hotList = ref<HotItem[]>([])
+
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+// 是否加载中标记
+const isLoading = ref(false)
+
 // 页面加载
 onLoad(async () => {
-  getHomeBannerData()
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
 })
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
-  <uni-card
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
+  <!-- 热门推荐 -->
+  <HotPanel :list="hotList" />
+
+  <!-- <uni-card
     title="基础卡片"
     sub-title="副标题"
     extra="额外信息"
@@ -30,9 +60,22 @@ onLoad(async () => {
   >
     <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
   </uni-card>
-  <view class="index">index</view>
+  <view class="index">index</view> -->
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+.viewport {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
+}
 </style>
